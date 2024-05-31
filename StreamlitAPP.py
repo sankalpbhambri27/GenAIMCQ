29609db1fac19df31e58b5eb8cc82c2249577bf6
Sankalp <sankalpinsa@gmail.com>
2024-05-31 17:07:21 -0500

    updated code to use lower case

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -11,7 +11,7 @@ from src.mcqgenerator.logger import logging
 
 #loading json file
 with open('Response.json', 'r') as file:
-    RESPONSE_JSON = json.load(file)
+    response_json = json.load(file)
 
 
 #creating a title for the app
@@ -41,7 +41,7 @@ with st.form("user input"):
                         "number": mcq_count,
                         "subject":subject,
                         "tone": tone,
-                        "RESPONSE_JSON": json.dumps(RESPONSE_JSON)
+                        "response_json": json.dumps(response_json)
                             }
                     )
                 #st.write(response)
--- src/mcqgenerator/mcqgenerator.py
+++ src/mcqgenerator/mcqgenerator.py
@@ -1,9 +1,9 @@
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+from langchain_community.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
-from langchain.callbacks import get_openai_callback
+from langchain_community.callbacks.manager import get_openai_callback
 import os
 import json
 import pandas as pd
@@ -20,19 +20,19 @@ print(key)
 llm=ChatOpenAI(openai_api_key=key,model_name="gpt-3.5-turbo",temperature=0.7)
 
 with open("/Users/user/code/genai/GenAIMCQ/Response.json","r") as f:
-    RESPONSE_JSON=json.load(f)
+    response_json=json.load(f)
 
-print(RESPONSE_JSON)
+print(response_json)
 
 TEMPLATE="""
 Text:{text}
 You are an expert MCQ maker. Given the above text, it is your job to \
 create a quiz  of {number} multiple choice questions for {subject} students in {tone} tone. 
 Make sure the questions are not repeated and check all the questions to be conforming the text as well.
-Make sure to format your response like  RESPONSE_JSON below  and use it as a guide. \
+Make sure to format your response like  response_json below  and use it as a guide. \
 Ensure to make {number} MCQs
-### RESPONSE_JSON
-{RESPONSE_JSON}
+### response_json
+{response_json}
 
 """
 
@@ -62,5 +62,5 @@ review_chain=LLMChain(llm=llm, prompt=quiz_evaluation_prompt, output_key="review
 
 
 
-generate_evaluate_chain=SequentialChain(chains=[quiz_chain,review_chain],input_variables=["text", "number", "subject", "tone", "RESPONSE_JSON"],
+generate_evaluate_chain=SequentialChain(chains=[quiz_chain,review_chain],input_variables=["text", "number", "subject", "tone", "response_json"],
                                         output_variables=["quiz", "review"], verbose=True,)
